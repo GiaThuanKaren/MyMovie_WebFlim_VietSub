d0065064194c9716f5569adf5460e3f8e0cb6737
GiaThuanKaren <GiaThuanNguyen213@gmail.com>
2022-07-01 20:38:07 +0700

    Add : Find Movie by Keyword 1/7/2022

--- Backend/src/Routes/routes.js
+++ Backend/src/Routes/routes.js
@@ -1,8 +1,11 @@
 const FitlerCatologeCrawl = require("./catologe");
 const Movie= require('./movie')
+const Find = require("./find")
 const Routes = function (app) {
   app.use("/fliterlist", FitlerCatologeCrawl);
   app.use("/movie",Movie)
+  app.use("/find",Find)
+  
 };
 
 module.exports = Routes;
