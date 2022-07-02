a947536bbb2e5efe85058bda72a67c33a7e155d6
GiaThuanKaren <GiaThuanNguyen213@gmail.com>
2022-07-02 17:06:49 +0700

    Call api and display to UI successfully 2/7/2022

--- Frontend/clientui/Components/MovieItem/MovieItem.js
+++ Frontend/clientui/Components/MovieItem/MovieItem.js
@@ -1,8 +1,17 @@
+import { Grid } from "@mui/material"
 import style from "./MovieItem.module.css"
 
-function MovieItem() {
+function MovieItem({slug,title,id,year,OriginalName}) {
   return (
-    <div>MovieItem</div>
+    <Grid item lg={3} md={3}>
+        <div className={`${style.MovieItemContainer}`}>
+            <div className={`${style.ImageMovieItem}`} style={{backgroundImage:`url(https://img.ophim.tv/uploads/movies/${slug}-thumb.jpg)`}}>
+
+            </div>
+            <p>{title}-({OriginalName})</p>
+            <p>{year}</p>
+        </div>
+    </Grid>
   )
 }
 
--- Frontend/clientui/Components/RightSideBar/RightSideBar.js
+++ Frontend/clientui/Components/RightSideBar/RightSideBar.js
@@ -1,12 +1,34 @@
 import { Grid } from "@mui/material";
-import Link from "next/link";
 import style from "./RightSideBar.module.css";
 import Pagination from "@mui/material/Pagination";
+import Link from "next/link";
 import Stack from "@mui/material/Stack";
+import { useSelector } from "react-redux";
+// import { Link, MemoryRouter, Route, Routes, useLocation } from 'react-router-dom';
+import PaginationItem from '@mui/material/PaginationItem';
+import MovieItem from "../MovieItem/MovieItem";
 function RightSideBar() {
+  const { CurListMovie } = useSelector((state) => state);
+  const { items, pagination } = CurListMovie;
+  console.log(items, 9);
   return (
     <Grid item lg={9} md={9} sm={12}>
-      <Grid container></Grid>
+      <Grid container>
+        {items && items.length > 0
+          ? items.map((item, idx) => {
+              return (
+                <MovieItem
+                  year={item.year}
+                  slug={item.slug}
+                  OriginalName={item.origin_name}
+                  id={item._id}
+                  title={item.name}
+                />
+              );
+            })
+          : null}
+        {/* MovieItems */}
+      </Grid>
       <Stack spacing={2}>
         <Grid container justifyContent={"center"}>
           <Pagination count={11} defaultPage={6} boundaryCount={2} />
